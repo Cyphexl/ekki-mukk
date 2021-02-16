a381fd8affaaa5e9401fd79758b6c49520541e10
Tzingtao Chow <Cyphexl@outlook.com>
2021-02-16 22:23:03 +0800

    style-fixes

--- themes/hugo/source/js/scrollbar.js
+++ themes/hugo/source/js/scrollbar.js
@@ -6,7 +6,7 @@ if (!is_touch_device()) {
     document.addEventListener("DOMContentLoaded", function () {
         //The first argument are the elements to which the plugin shall be initialized
         //The second argument has to be at least a empty object or a object with your desired options
-        OverlayScrollbars(document.querySelectorAll('.highlight table, .katex-display'), {
+        OverlayScrollbars(document.querySelectorAll('.highlight table'), {
             className: "os-theme-minimal-dark",
             scrollbars: {
                 autoHide: "leave",
